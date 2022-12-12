22ff92446ab08ca15f91d6347e0d6e6408902efb
tura-lab <tiruzertsedeke25@gmail.com>
2022-12-12 19:00:30 +0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -16,7 +17,8 @@ import { HorSliderComponent } from './components/hor-slider/hor-slider.component
     HorSliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
